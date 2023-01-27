18f3530602b598b573e8d6d1005dfa82364edce5
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-01-27 08:56:31 -0800

    chore: update site to be modern

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,7 @@
-import { ListOfShame } from "../components/ListOfShame.tsx";
-
 export default function Home() {
   return (
     <div class="">
-      <section
-        class="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-r from-pink-200 via-red-200 to-yellow-200"
-      >
+      <section class="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-r from-pink-200 via-red-200 to-yellow-200">
         <img
           src="/logo.svg"
           width="200px"
@@ -15,46 +11,129 @@ export default function Home() {
           Welcome to duno.land
         </h1>
         <p class="text-lg text-center">
-          Why Deno might not be ready for you
+          Why Deno might not be ready for you and what we are doing about it
         </p>
       </section>
-      <section
-        class="bg-gradient-to-r from-pink-200 via-red-200 to-yellow-200 relative h-8"
-      >
+      <section class="bg-gradient-to-r from-pink-200 via-red-200 to-yellow-200 relative h-8">
         <img
           src="/assets/wave.svg"
           class="w-full min-w-lg absolute -bottom-1"
           alt="a small wave graphic"
         />
       </section>
-      <section
-        class="mx-auto border-b py-16 px-4 max-w-2xl flex flex-col gap-4"
-      >
-        <h2 class="text-3xl text-center flex flex-col sm:flex-row items-center gap-4">A few words about duno.land  <iframe src="https://ghbtns.com/github-btn.html?user=lino-levan&repo=duno.land&type=star&count=true&size=large" scrolling="0" width="170" height="30" title="GitHub" /></h2>
+      <section class="mx-auto border-b py-16 px-4 max-w-2xl flex flex-col gap-4">
+        <h2 class="text-3xl text-center flex flex-col sm:flex-row items-center gap-4">
+          Our projects
+        </h2>
+        <p>
+          While we do think Deno isn't quite ready for prime time yet, we are
+          actively working on this by building deno-first modules under the
+          "dunosaurs" github organization.
+        </p>
+        <div class="grid grid-cols-3 gap-4">
+          <div class="border p-2 rounded">
+            <h3 class="text-xl underline">
+              <a href="https://github.com/dunosaurs/stats">x/stats</a>
+            </h3>
+            <p>
+              A statistics / data science library for Deno
+            </p>
+          </div>
+          <div class="border p-2 rounded">
+            <h3 class="text-xl underline">
+              <a href="https://github.com/dunosaurs/diff">x/diff</a>
+            </h3>
+            <p>
+              A simple and fast typescript diff implementation for Deno
+            </p>
+          </div>
+          <div class="border p-2 rounded">
+            <h3 class="text-xl underline">
+              <a href="https://github.com/dunosaurs/color">x/color</a>
+            </h3>
+            <p>
+              A simple, fast, and comprehensive color library for Deno.
+            </p>
+          </div>
+          <div class="border p-2 rounded">
+            <h3 class="text-xl underline">
+              <a href="https://github.com/dunosaurs/color">x/slug</a>
+            </h3>
+            <p>
+              Deno library for converting arbitrary strings into valid slugs
+            </p>
+          </div>
+          <div class="border p-2 rounded">
+            <h3 class="text-xl underline">
+              <a href="https://github.com/dunosaurs/gtts">x/gtts</a>
+            </h3>
+            <p>
+              A simple deno text-to-speech library using the google translate
+              api.
+            </p>
+          </div>
+          <div class="border p-2 rounded">
+            <h3 class="text-xl underline">
+              <a href="https://github.com/dunosaurs/supports_color">
+                x/supports_color
+              </a>
+            </h3>
+            <p>
+              A Deno port of chalk's supports-color
+            </p>
+          </div>
+          <div class="border p-2 rounded">
+            <h3 class="text-xl underline">
+              <a href="https://github.com/dunosaurs/nostr">x/nostr</a>
+            </h3>
+            <p>
+              A Deno implementation of a nostr client and server.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="mx-auto border-b py-16 px-4 max-w-2xl flex flex-col gap-4">
+        <h2 class="text-3xl text-center flex flex-col sm:flex-row items-center gap-4">
+          A few words about duno.land{" "}
+          <iframe
+            src="https://ghbtns.com/github-btn.html?user=lino-levan&repo=duno.land&type=star&count=true&size=large"
+            scrolling="0"
+            width="170"
+            height="30"
+            title="GitHub"
+          />
+        </h2>
         <p>
           The javascript community, and I personally, really want to see Deno
           succeed. This site is not intended to "dunk" on Deno in any way. It's
           simply my way of listing what I see as its current shortcomings and
           what I hope it can improve on.
         </p>
+      </section>
+      <section class="mx-auto border-b py-16 px-4 max-w-2xl flex flex-col gap-4">
+        <h2 id="core-issues" class="text-3xl">Updates</h2>
         <p>
-          I'm going to list my issues in two seperate categories: Core and
-          Community.
+          The fact that this page needs updating is a fantastic sign that Deno
+          is headed in the right direction.
         </p>
+        <h3 id="module-ecosystem" class="text-2xl">Deno isn't mature</h3>
         <p>
-          Core issues are issues with the Deno runtime itself, or projects by
-          the Deno organization. Community issues is basically just a list of
-          large packages that don't support Deno at all right now. While
-          community issues are not technically a problem with Deno, they are a
-          huge roadblock to adoption.
+          <a class="underline">
+            Lockfile should be written by default
+          </a>{" "}
+          - Deno now writes lockfiles by default 🎉. This is a great step
+          forward in the world of potential supply-chain attacks.
         </p>
+        <h3 id="module-ecosystem" class="text-2xl">Module Ecosystem</h3>
         <p>
-          Now, it's time to get to the list.
+          Deno now does have nearly the module support of node with the release
+          and stabilization of npm specifiers. While the compatibility isn't
+          100% there, and there are still some issues with the compat layer's
+          implementation of things like websocket and https, it is now perfectly
+          usuable and no longer the huge problem it was.
         </p>
       </section>
-      <section
-        class="mx-auto border-b py-16 px-4 max-w-2xl flex flex-col gap-4"
-      >
+      <section class="mx-auto border-b py-16 px-4 max-w-2xl flex flex-col gap-4">
         <h2 id="core-issues" class="text-3xl">Core Issues</h2>
         <h3 id="deno-is-slow" class="text-2xl">Deno is slow</h3>
         <p>
@@ -87,23 +166,6 @@ export default function Home() {
           taking the performance crown handily. This becomes very important for
           the future of Deno.
         </p>
-        <h3 id="deno-isnt-stable" class="text-2xl">Deno isn't stable</h3>
-        <p>
-          Deno's standard library{" "}
-          <a
-            class="underline"
-            href="https://deno.land/manual@v1.24.3/runtime/stability"
-          >
-            isn't stable yet
-          </a>. This is, in my opinion, one of the biggest blockers for Deno
-          adoption in enterprise. People can't afford to take the risk this
-          brings along, even if it's imaginary. Stablizing the standard library
-          would be a huge step to getting company adoption.
-        </p>
-        <p>
-          None of this is to mention that there are parts of the Deno namespace
-          that are still unstable.
-        </p>
         <h3 id="deno-isnt-mature" class="text-2xl">Deno isn't mature</h3>
         <p>
           People say this a lot but I want to be a little more concrete about
@@ -122,7 +184,7 @@ export default function Home() {
           - The Deno namespace has a few methods that shouldn't be there. Using
           any Deno namespace call instantly makes your code incompatible with
           the browser and thus isn't great. Moving any code that *should* be
-          browser-compatible to a different module would fix this.
+          browser-compatible to the standard library would fix this.
         </p>
         <p>
           <a
@@ -138,18 +200,6 @@ export default function Home() {
         <p>
           <a
             class="underline"
-            href="https://github.com/denoland/deno/issues/5501"
-          >
-            Lockfile should be written by default
-          </a>{" "}
-          - Deno doesn't write a lockfile by default. While many argue that this
-          is a good thing, I completely disagree. Lockfiles are what keep modern
-          dependecy-full apps alive and not creating them by default encourages
-          people to not use them.
-        </p>
-        <p>
-          <a
-            class="underline"
             href="https://github.com/denoland/deno/issues/11186"
           >
             Default to h and Fragment for JSX
@@ -158,12 +208,6 @@ export default function Home() {
           supposed "first class" support for typescript, this not being nailed
           down isn't great.
         </p>
-        <p>
-          That's enough of that. We're going to move on to the community issues.
-        </p>
-      </section>
-      <section class="mx-auto py-16 px-4 max-w-2xl flex flex-col gap-4">
-        <h2 id="community-issues" class="text-3xl">Community Issues</h2>
         <h3 id="stack-overflow" class="text-2xl">Stack Overflow</h3>
         <p>
           Deno has nearly no Stack Overflow history, especially in comparison to
@@ -176,7 +220,7 @@ export default function Home() {
           >
             A search for Deno on SO
           </a>{" "}
-          results in just under 2,000 questions.
+          results in just over 2,000 questions.
         </p>
         <p>
           <a
@@ -185,28 +229,11 @@ export default function Home() {
           >
             In node?
           </a>{" "}
-          Over 1,000,000!
+          Over 1,100,000!
         </p>
         <p>
           If you have a problem with Deno, there is a very high likelyhood that
           you will be the first person on the internet with that specific issue.
-        </p>
-        <h3 id="module-ecosystem" class="text-2xl">Module Ecosystem</h3>
-        <p>
-          Everyone knew this was going to be brought up at some point. Deno does
-          not have nearly the same module support as Node. While the --compat
-          flag does bring it a long way, it's still far from the nearly infinite
-          libraries that exist for Node.
-        </p>
-        <p>
-          Every major library should eventually be compatible with Deno. If they
-          aren't they go on the list of shame. Feel free to contribute on our
-          github.
-        </p>
-        <h3 id="list-of-shame" class="text-2xl">List of Shame</h3>
-        <ListOfShame />
-        <p>
-          This list is up to date as of 22/08/2022.
         </p>
       </section>
       <footer class="bg-coolGray-100 px-4 py-8 flex justify-center gap-4">
@@ -221,7 +248,10 @@ export default function Home() {
         </p>
         <p>•</p>
         <p>
-          <a class="underline" href="https://linolevan.com/blog/duno_land_announcement">
+          <a
+            class="underline"
+            href="https://linolevan.com/blog/duno_land_announcement"
+          >
             The blog post that started it all.
           </a>
         </p>
